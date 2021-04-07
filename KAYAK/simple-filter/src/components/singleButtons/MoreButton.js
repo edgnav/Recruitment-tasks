daa5aad7667f93795835d70736858f58daca3eed
unknown <edgarasnavicaks@gmail.com>
2021-04-07 23:02:01 +0300

    fixed buttons layout

--- KAYAK/simple-filter/src/components/singleButtons/MoreButton.js
+++ KAYAK/simple-filter/src/components/singleButtons/MoreButton.js
@@ -1,25 +1,35 @@
-import React from 'react';
+import React, { useState } from 'react';
 import { DropdownButton, Dropdown } from 'react-bootstrap';
 
 const MoreButton = () => {
+  const [rotate, setRotate] = useState(false);
+  const handleChange = () => {
+    rotate ? setRotate(false) : setRotate(true);
+  };
   return (
-    <button className="more-button">
+    <button
+      className="more-button"
+      onMouseOver={handleChange}
+      onMouseOut={handleChange}
+    >
       <div className="more-button-container">
         <div style={{ float: 'left', marginLeft: '16px' }}>More</div>
-
-        <svg
-          xmlns="http://www.w3.org/2000/svg"
-          width="14"
-          height="14"
-          fill="currentColor"
-          class="bi bi-chevron-down"
-          viewBox="0 0 16 16"
-        >
-          <path
-            fill-rule="evenodd"
-            d="M1.646 4.646a.5.5 0 0 1 .708 0L8 10.293l5.646-5.647a.5.5 0 0 1 .708.708l-6 6a.5.5 0 0 1-.708 0l-6-6a.5.5 0 0 1 0-.708z"
-          />
-        </svg>
+        <div className={rotate && 'rotate'}>
+          <svg
+            xmlns="http://www.w3.org/2000/svg"
+            width="14"
+            height="14"
+            fontWeight="bold"
+            fill="currentColor"
+            class="bi bi-chevron-down"
+            viewBox="0 0 16 16"
+          >
+            <path
+              fill-rule="evenodd"
+              d="M1.646 4.646a.5.5 0 0 1 .708 0L8 10.293l5.646-5.647a.5.5 0 0 1 .708.708l-6 6a.5.5 0 0 1-.708 0l-6-6a.5.5 0 0 1 0-.708z"
+            />
+          </svg>
+        </div>
       </div>
     </button>
   );
